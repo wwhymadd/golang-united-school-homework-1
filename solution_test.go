e4e842b4fd4fff4e301fa9ddfbe5880c4ece916f
wwhymadd <99489845+wwhymadd@users.noreply.github.com>
2022-02-15 03:14:37 +0300

    Update solution_test.go

--- solution_test.go
+++ solution_test.go
@@ -3,9 +3,9 @@ package solution_test
 import "testing"
 
 func TestGetMessage(t *testing.T) {
-	s := "Hello wrld"
+	s := "Hello world"
 	println(t)
 	if len(s) == 0 {
-		t.Errorf("Expected 'Hello wrld', but got %s ", s)
+		t.Errorf("Expected 'hello world', but got %s ", s)
 	}
 }
